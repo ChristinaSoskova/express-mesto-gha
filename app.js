77b82e436a96cf253f126df2016d7ffe179a5baf
Kristina Soskova <soskovachristina@yandex.ru>
2023-01-25 16:48:31 +0300

    Проект 14

--- app.js
+++ app.js
@@ -41,13 +41,13 @@ app.listen(PORT, () => {
 
 app.use(errors());
 
-// app.use((err, req, res, next) => {
-//   const { statusCode = 500, message } = err;
-//   res.status(statusCode).send({
-//     message: statusCode === 500 ? "На сервере произошла ошибка" : message,
-//   });
-//   next();
-// });
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({
+    message: statusCode === 500 ? "На сервере произошла ошибка" : message,
+  });
+  next();
+});
 
 mongoose.connect(DB);
 
